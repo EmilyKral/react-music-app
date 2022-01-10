2b551783d32ae8db37ca265ec84a1f470e0a2c25
EmilyKral <emilykral@gmail.com>
2022-01-10 17:08:01 +0000

    added header styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Music, About } from "./pages";
 import { Header } from "./layout";
+import "./App.css";
 
 function App() {
 	return (
--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./style.css";
 
 function Header() {
 	return (
 		<header>
-			<h1>Tessa Violet</h1>
+			<h1>TESSA VIOLET</h1>
 			<nav>
 				<NavLink to="/">About</NavLink>
 				<NavLink to="/albums">Albums</NavLink>
